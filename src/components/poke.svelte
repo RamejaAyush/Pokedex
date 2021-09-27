f0c535962d8637c55587aec58546b27ca859ad70
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-27 18:52:28 +0530

    added filtered pokemon

---
+++ src/components/poke.svelte
@@ -0,0 +1,76 @@
+<script>
+	export let poke;
+</script>
+
+<!-- <p>{poke.name}</p> -->
+<a href={`/poke/${poke.id}`} class="poke-ele">
+	<img src={poke.image} alt={poke.name} />
+	<h2>{poke.id}. {poke.name}</h2>
+</a>
+
+<style>
+	.poke-ele {
+		width: 25%;
+		height: 40vh;
+		border-radius: 15px;
+		text-decoration: none;
+		color: white;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		z-index: 1;
+	}
+	.poke-ele::after,
+	.poke-ele::before {
+		content: '';
+		position: absolute;
+		z-index: -1;
+		top: 0;
+		bottom: 5%;
+		left: 10%;
+		right: 0;
+		border-radius: 15px;
+		background-color: rgb(152, 23, 238);
+		transition: all 150ms ease-in-out;
+	}
+	.poke-ele:hover::after,
+	.poke-ele:hover::before {
+		bottom: 0%;
+		left: 0%;
+	}
+	.poke-ele img {
+		width: 80%;
+		height: 60%;
+		object-fit: cover;
+		margin: auto;
+	}
+	.poke-ele h2 {
+		height: 40%;
+		display: grid;
+		place-items: center;
+		text-align: center;
+	}
+	@media only screen and (max-width: 900px) {
+		.poke-ele {
+			width: 35%;
+		}
+	}
+	@media only screen and (max-width: 700px) {
+		.poke-ele {
+			width: 40%;
+		}
+	}
+	@media only screen and (max-width: 600px) {
+		.poke-ele {
+			width: 50%;
+		}
+	}
+	@media only screen and (max-width: 500px) {
+		.poke-ele {
+			width: 80%;
+		}
+		.poke-ele h2 {
+			font-size: 20px;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,22 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
+	import Poke from '../components/poke.svelte';
+
+	let search = '';
+	let filterPoke = [];
+
+	// reactive function
+	$: {
+		console.log(search);
+		if (search) {
+			// change filtered pokemon
+			filterPoke = $pokemon.filter((poke) =>
+				poke.name.toLowerCase().includes(search.toLowerCase())
+			);
+		} else {
+			filterPoke = [...$pokemon];
+		}
+	}
 </script>
 
 <svelte:head>
@@ -8,12 +25,45 @@
 
 <h1>Svelte Kit Pokedex</h1>
 
-{#each $pokemon as poke}
-	<p>{poke.name}</p>
-{/each}
+<div class="textSearch">
+	<input type="text" name="search" id="search" placeholder="Search Pokemon" bind:value={search} />
+</div>
+
+<div class="pokeCont">
+	{#each filterPoke as poke}
+		<Poke {poke} />
+	{/each}
+</div>
 
 <style>
 	h1 {
 		text-align: center;
+		font-size: 42px;
+	}
+	.pokeCont {
+		min-height: 100vh;
+		width: 100%;
+		padding: 5rem;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 55px;
+		align-items: center;
+		justify-content: center;
+		margin: 0 auto;
+	}
+
+	.textSearch {
+		width: 100%;
+		height: 20vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.textSearch input {
+		width: 40%;
+		padding: 10px;
+		outline: none;
+		border-radius: 5px;
 	}
 </style>
